6d69054fcba1997396251a0415c8a76188ad261a
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-06-23 14:28:06 +0200

    update authenticate middlewares

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,12 +6,11 @@ const { SECRET_KEY } = process.env;
 
 const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
-  const { bearer, token } = authorization.split(" ");
+  const [bearer, token] = authorization.split(" ");
 
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
-
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
