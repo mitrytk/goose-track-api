0a5949a1288a53abecec58da088ca44877a2eca3
Oleksandr Makogin <sashamakohin@gmail.com>
2023-06-28 19:27:40 +0300

    chenged-schema-message

--- schemas/statistics.js
+++ schemas/statistics.js
@@ -4,7 +4,7 @@ const statisticsSchema = Joi.object({
   date: Joi.string()
     .pattern(/^\d{4}-\d{2}-\d{2}$/)
     .messages({
-      "string.pattern.base": "Must be string with format e.g: YYYY MM DD",
+      "string.pattern.base": "Must be string with format e.g: YYYY-MM-DD",
     })
     .required(),
 });
