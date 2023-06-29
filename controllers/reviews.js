f92e544baba423b3a04d5f82b904a976b302fef7
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-06-29 21:20:05 +0300

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -7,18 +7,18 @@ const getReviews = async (req, res, next) => {
     const skip = (page - 1) * limit;
 
     const result = await Review.find({}, "", {skip, limit});
-    res.status(200).json({result});
+    res.status(200).json(result);
 }
 
 const getReview = async (req, res, next) => {
     const {id} = req.params;
-    const result = await Review.find(id);
+    const result = await Review.findById(id);
 
     if(!result) {
         throw HttpError(404, "Not found")
       }
 
-    res.status(200).json({result});
+    res.status(200).json(result);
 }
 
 const addReview = async (req, res, next) => {
@@ -27,7 +27,7 @@ const addReview = async (req, res, next) => {
 
     const result = await Review.create({...req.body, _id, owner});
 
-    res.status(200).json({result});
+    res.status(200).json(result);
 }
 
 const editReview = async (req, res, next) => {
@@ -38,7 +38,7 @@ const editReview = async (req, res, next) => {
         throw HttpError(404, "Not found")
       }
 
-    res.status(200).json({result});
+    res.status(200).json(result);
 }
 
 const deleteReview = async (req, res, next) => {
