64229c19d8a1f41703582aa093a3558902c81e10
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-07-03 22:16:17 +0300

    add google

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,6 +20,9 @@ router.post("/login", volidateBody(loginShema), ctrl.login);
 router.patch("/password", authenticate, ctrl.changePassword);
 router.post("/logout", authenticate, ctrl.logout);
 router.get("/current", authenticate, ctrl.getCurrent);
+router.get("/google", ctrl.googleAuth);
+router.get("/google-redirect", ctrl.googleRedirect);
+router.get("/loginGoogle", ctrl.loginGoogle);
 router.patch(
   "/update",
   authenticate,
